bdd373a52d4379969f025b8216505d92125b47fe
JoyDaheeCha <daheecha0612@gmail.com>
2022-02-06 13:38:50 +0900

    commit: JPQL 실행시 영속성 컨텍스트에 반영된다.

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,8 +16,18 @@ public class JpaMain {
 
         try {
             Member member = new Member();
-            member.setUsernmae("member1");
+            member.setUsernmae("dahee");
+            member.setAge(10);
             em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            List<Member> result = em.createQuery("select m from Member m", Member.class)
+                    .getResultList();
+
+            Member findMember = result.get(0);
+            findMember.setAge(20);
 
             tx.commit();
         } catch (Exception e) {
